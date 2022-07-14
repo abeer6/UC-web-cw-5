d93c15f1da0c967a4aebfcbfe0484280bf0fd704
abeer6 <108021869+abeer6@users.noreply.github.com>
2022-07-14 05:55:35 +0300

    my task

---
+++ script.js
@@ -0,0 +1,81 @@
+function BMI(weight,height){
+    
+    return  weight/(height*height);
+}
+    
+    let x =BMI(60,1.70);
+    
+    console.log(x);
+
+// الس}ال الثاني//////////////////////////////
+
+    function Status(bmi){
+
+      if (bmi<18.5) {
+          console.log(`لديك نقص في الوزن`);}
+
+         else if ((bmi<25 ) && (bmi>=18.5)){
+           console.log(`وزنك صحي`);}
+          else  (bmi>=25) {
+           console.log(`لديك زيادة في الوزن`);}
+         }
+        
+         ///السؤال الثالث
+         
+   function calaulate{
+
+   }
+   let bmi=BMI();
+   let desc=Status();
+
+let weight =document.getElementById('weight').value;
+let height =document.getElementById('height').value;
+
+   
+
+
+
+
+
+
+
+
+    //console.log()
+//let weight=prompt("اخل الوزن");
+//let height=prompt("ادخل الطول");
+////let button=document.getElementById(button);
+//button.addEventListener('click'),function BMI(weight,height){
+    
+    //let weight =document.getElementById('weight').value;
+    //let height =document.getElementById('height').value;
+    //console.log(weight);
+    //console.log(height);
+    //let bmi =  weight/(height*height);
+    //document.getElementById("result").value=bmi;
+
+    //console.log('value of='+weight+'value of ='+height)
+
+    //let bmi =  weight/(height*height);
+    //let res=Math.random(result);
+  //let res =document.getElementById("result").value=res;
+    //console.log( weight/(height*height));
+
+
+   // console.log(input.value);
+
+  //  return result;
+
+//BMI(weight,height);
+
+//function Status(bmi){
+
+   // if (bmi<18.5) {
+     //   console.log("لديك نقص في الوزن");}
+      //  else if {(bmi<25 )&&(bmi>=18.5);
+      //  console.log("وزنك صحي");
+       // else if (bmi>=25);
+       // console.log("لديك زيادة في الوزن");
+    
+
+
+//let button =document.getAnimations('')
